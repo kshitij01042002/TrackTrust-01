294e0e530c3731a84f0c8b91655f1d02ff88b9d0
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-09-16 00:36:58 +0530

    customer and sell

--- src/components/Sell.js
+++ src/components/Sell.js
@@ -120,41 +120,21 @@ const data = [
 
     return(
 
-        <section class="text-gray-400 bg-gray-900 body-font">
-        <div class="container px-5 py-24 mx-auto flex flex-wrap">
-          <div class="lg:w-3/3 mx-auto">
-            <div class="flex flex-wrap w-full bg-gray-800 py-32 px-10 relative mb-4">
-            {/* <table className="table-auto w-full text-left whitespace-no-wrap">
-              <thead>
-                <tr>
-                  <th className="px-4 py-3 title-font tracking-wider font-medium text-white text-sm bg-gray-800 rounded-tl rounded-bl">Product</th>
-                  <th className="px-4 py-3 title-font tracking-wider font-medium text-white text-sm bg-gray-800">Price</th>
-                  <th className="w-10 title-font tracking-wider font-medium text-white text-sm bg-gray-800 rounded-tr rounded-br"></th>
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((item, index) => (
-                  <tr key={index}>
-                    <td className="px-4 py-3">{item.product}</td>
-                    <td className="px-4 py-3 text-lg text-white">${item.price}</td>
-                    <td className="px-4 py-3 text-lg text-white">${item.id}</td>
-                    <td className="w-10 text-center">
-  <input type="checkbox" name="checkbox1" value={item.id}></input>
-   </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table> */}
-
-<MaterialReactTable columns={columns} data={data} />
-      <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"  onClick={getCheckedValues}>Button</button>
-            
-            </div>
-
-          </div>
+      <section className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-white">SELL Table</h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Place the orders from this page</p>
         </div>
-      </section>
-
+        <div className="lg:w-2/3 w-full mx-auto overflow-auto">
+          <MaterialReactTable
+            columns={columns}
+            data={data}
+            cellStyle={{ background: 'black'}}
+          />
+        </div>
+      </div>
+    </section>
 )};
 
 export default Sell;
